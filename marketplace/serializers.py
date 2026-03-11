36b3b542083baf8f82ef0f71f43e979bd19354ee
sashang magar <sashangmagar20@gmail.com>
2026-03-11 22:58:15 +0545

    serializer validation Order

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -14,15 +14,43 @@ class SimpleProductSerializer(serializers.ModelSerializer):
         fields = ['id', 'name' , 'unit_price']
 
 class OrderItemSerializer(serializers.ModelSerializer):
-    product = SimpleProductSerializer()
+    product = SimpleProductSerializer(read_only=True)
+    total_price = serializers.SerializerMethodField() #total_price is not stored in the database
     class Meta:
         model = OrderItem
-        fields = [ 'id' ,'product' , 'quantity' , 'unit_price']
+        fields = [ 'id' ,'product' , 'quantity' , 'unit_price','total_price']
+
+    def get_total_price(self , order_item:OrderItem):
+        return order_item.quantity * order_item.unit_price    
+
+    # def validate_quantity(self , value):
+    #     if value <= 0 :
+    #         raise serializers.ValidationError('Quantity must be greater than zero.')  
+    #     return value
+    def validate(self, attrs):
+
+        product = attrs.get("product")
+        quantity = attrs.get("quantity")
+        request = self.context.get("request")
+
+        if request and product.farmer == request.user:
+            raise serializers.ValidationError(
+                "You cannot order your own product."
+            )
+
+        if quantity > product.stock:
+            raise serializers.ValidationError(
+                "Not enough stock available."
+            )
+
+        return attrs
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True)
+    items = OrderItemSerializer(many=True,read_only=True)
     class Meta :
         model = Order
         fields = ['id','user','payment_status' ,'payment_method','delivery_address','created_at', 'updated_at','items' ]
         read_only_fields = ['id' , 'user' ,'created_at', 'updated_at']
+
+    #def validate_user(self , value)    
                 
